87b7b7b864b61b426d608c38bd75d4a6372612d7
asifjack <mdasifwr@gmail.com>
2022-02-27 21:09:10 +0530

    add sql file

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿Create Database MultiLayerCrud_Db
+
+Use MultiLayerCrud_Db
+
+Create table Employees(
+ id int not null identity ,
+ FirstName varchar(100),
+ LastName varchar(100),
+ Email varchar(100),
+ Gender varchar(100)
+) 
+insert into Employees values
+('Asif','Alam','asif@gmail.com','male'),
+('Soha','Khan','soahkhan@gmail.com','female')
+
+select * from Employees;
